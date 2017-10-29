e127ad0ad03ca8273faae785427232d9505bc2c8
Brendan Hart <brendanrayhart@gmail.com>
2017-10-29 23:47:01 +0000

    Adding comments and restricting port number further.

--- src/main.c
+++ src/main.c
@@ -20,16 +20,19 @@ int main(int argc, char **argv) {
     assert(argv[0] && *argv[0]);
     program_name = argv[0];
 
+    // Check we have the disired number of arguments
     if(argc != 5) {
         fprintf(stderr, "%s: usage: %s -p <port> -d <web_root>\n", program_name, program_name);
         exit(1);
     }
 
+    // Make sure they all exist
     assert(argv[1] && *argv[1]);
     assert(argv[2] && *argv[2]);
     assert(argv[3] && *argv[3]);
     assert(argv[4] && *argv[4]);
 
+    // Process -p and -d flags, in any order
     int i;
     for(i = 1; i < argc; i++) {
         if(i % 2 == 0) {
@@ -46,29 +49,35 @@ int main(int argc, char **argv) {
         }
     }
 
-    if(port > 65535) {
-        fprintf(stderr, "%s: port number should be less than 65535.\n", program_name);
+    // Check the port is valid and can be listened on without sudo
+    if(port > 65535 || port < 1024) {
+        fprintf(stderr, "%s: port number should in the range 1024 <= port < 65535.\n", program_name);
         exit(1);
     }
 
+    // Initialise SSL
     OpenSSL_add_all_algorithms();
     SSL_load_error_strings();
     ERR_load_BIO_strings();
     SSL_library_init();
 
+    // Get the listen socket
     socket_fd = get_listen_socket(port);
     if(socket_fd < 0) {
         fprintf(stderr, "%s: error getting listen socket.", program_name);
         exit(1);
     }
 
+    // Handle bad pipe signal
     signal(SIGPIPE, SIG_IGN);
 
+    // Handle the listen socket
     if(handle_listen_socket(socket_fd, root_dir) != 0) {
         fprintf(stderr, "%s: error processing listen socket.", program_name);
         exit(1);
     }
 
+    // Cleanup after server
     ERR_free_strings();
     EVP_cleanup();
 
